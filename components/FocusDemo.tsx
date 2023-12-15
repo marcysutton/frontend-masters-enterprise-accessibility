d51bcb48e87a20d7a60d662ffa832924b0cd4b8e
Marcy Sutton-Todd <marcy.sutton.todd@principlestudios.com>
2023-12-14 20:00:53 -0600

    A11y in JS updates

---
+++ components/FocusDemo.tsx
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+const FocusDemo = () => {
+	const div1Ref = useRef<HTMLDivElement | null>(null);
+	const div2Ref = useRef<HTMLDivElement | null>(null);
+
+	return (
+		<div className="grid grid-cols-2 gap-6 w-full my-4">
+			<figure>
+				<div>
+					<button
+						className="bg-[#333] text-sm text-white font-bold p-2 rounded"
+						onClick={() => {
+							div1Ref.current && div1Ref.current.focus();
+						}}>
+						Click for demo 1
+					</button>
+					<div ref={div1Ref} className="focus-demo bg-white text-black p-2 my-2" tabIndex={-1}>
+						Click me
+					</div>
+				</div>
+				<figcaption className="italic">Wrapper `div[tabIndex="-1"]` with :focus</figcaption>
+			</figure>
+			<figure>
+				<div>
+					<button
+						className="bg-[#333] text-sm text-white font-bold p-2 rounded"
+						onClick={() => {
+							div2Ref.current && div2Ref.current.focus();
+						}}>
+						Click for demo 2
+					</button>
+					<div ref={div2Ref} className="focus-visible-demo bg-white text-black p-2 my-2" tabIndex={-1}>
+						Click me too
+					</div>
+				</div>
+				<figcaption className="italic">Wrapper `div[tabIndex="-1"]` with :focus-visible</figcaption>
+			</figure>
+		</div>
+	);
+};
+
+export default FocusDemo;
